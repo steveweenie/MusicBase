6e65d75dc90beb5883b6bc61b0d854c7cffcc44f
Abraham Steve Colina <130194837+steveweenie@users.noreply.github.com>
2025-11-17 09:44:13 -0600

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -22,6 +22,38 @@ BEGIN
 END //
 DELIMITER ;
 
-SELECT * FROM artists WHERE artist_id = 1;
+-- SELECT * FROM artists WHERE artist_id = 1;
 
-CALL p_show_followers_group(1);
+-- CALL p_show_followers_group(1);
+
+DROP PROCEDURE IF EXISTS p_song_by_genre;
+
+DELIMITER //
+CREATE PROCEDURE p_song_by_genre(IN genre_name_param VARCHAR(100))
+BEGIN
+    SELECT s.song_name, s.song_duration, s.release_date
+    FROM songs s
+    JOIN song_genres sg ON s.song_id = sg.song_id
+    JOIN genres g ON sg.genre_id = g.genre_id
+    WHERE g.genre_name = genre_name_param;
+END //
+DELIMITER ;
+
+-- CALL p_song_by_genre('Rock');
+
+DELIMITER //
+CREATE PROCEDURE p_album_by_genre(IN genre_name_param VARCHAR(100))
+BEGIN
+    SELECT DISTINCT a.album_name, a.release_date, a.label
+    FROM albums a
+    JOIN songs s ON a.album_id = s.album_id
+    JOIN song_genres sg ON s.song_id = sg.song_id
+    JOIN genres g ON sg.genre_id = g.genre_id
+    WHERE g.genre_name = genre_name_param;
+END //
+DELIMITER ;
+
+-- CALL p_album_by_genre('Rock');
+
+
+
